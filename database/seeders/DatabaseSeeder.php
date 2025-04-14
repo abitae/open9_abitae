5692b2e1149466b9427f60933262bb242285b3e7
Abel Arana <abel.arana@hotmail.com>
2025-04-14 16:04:04 -0500

    actualizacion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
 use App\Models\Blog\Comment;
-use App\Models\Blog\Media;
 use App\Models\Blog\Post;
 use App\Models\Blog\PostImage;
 use App\Models\Blog\Tag;
@@ -28,32 +26,32 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Create sample blog posts with tags, comments, and media
-        $user = User::first();
-        
+        //$user = User::first();
+
         // Create tags
-        $tags = Tag::factory(10)->create();
-        
+        //$tags = Tag::factory(10)->create();
+
         // Create posts with relationships
-        Post::factory(20)
+        /* Post::factory(20)
             ->create(['user_id' => $user->id])
             ->each(function ($post) use ($tags, $user) {
                 // Attach random tags to each post (between 1-4 tags)
                 $post->tags()->attach(
                     $tags->random(rand(1, 4))->pluck('id')->toArray()
                 );
-                
+
                 // Add 0-5 comments to each post
                 Comment::factory(rand(0, 5))
                     ->create([
                         'post_id' => $post->id,
                         'user_id' => $user->id,
                     ]);
-                
+
                 // Add 0-3 media items to each post
                 PostImage::factory(rand(0, 3))
                     ->create([
                         'post_id' => $post->id,
                     ]);
-            });
+            }); */
     }
 }
